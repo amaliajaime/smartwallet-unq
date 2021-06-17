b85cbdd0ead076bf7b97ff01d292ce0df8c6f13f
Amalia Jaime <amaliajaime3@gmail.com>
2021-06-17 18:59:42 -0300

    .

--- src/main/java/app/SmartWalletApplication.java
+++ src/main/java/app/SmartWalletApplication.java
@@ -3,14 +3,17 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.nio.file.FileStore;
 
 @SpringBootApplication
 @ComponentScan
 public class SmartWalletApplication extends SpringBootServletInitializer {
 
-        public void main(String[] args) {
+        public static void main(String[] args) {
             SpringApplication.run(SmartWalletApplication.class, args);
         }
 
